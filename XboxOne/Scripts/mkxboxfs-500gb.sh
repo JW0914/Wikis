82ca9cdee314cbffd20792aa590890e02980eb43
JW0914 <jw0914@outlook.com>
2016-04-23 13:15:48 -0500

    Wiki Collection
    Updated wiki to make it more fluid and easier to follow

---
+++ XboxOne/Scripts/mkxboxfs-500gb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DEV=/dev/sdb
+
+parted -s "$DEV" mktable gpt
+parted -s "$DEV" mkpart primary ntfs 1.00MiB 41985MiB
+parted -s "$DEV" name 1 "\"Temp Content\""
+mkntfs -q "${DEV}1" -f -L "Temp Content"
+
+parted -s "$DEV" mkpart primary ntfs 41985MiB 415745MiB
+parted -s "$DEV" name 2 "\"User Content\""
+mkntfs -q "${DEV}2" -f -L "User Content"
+
+parted -s "$DEV" mkpart primary ntfs 415745MiB 456705MiB
+parted -s "$DEV" name 3 "\"System Support\""
+mkntfs -q "${DEV}3" -f -L "System Support"
+
+parted -s "$DEV" mkpart primary ntfs 456705MiB 468993MiB
+parted -s "$DEV" name 4 "\"System Update\""
+mkntfs -q "${DEV}4" -f -L "System Update"
+
+parted -s "$DEV" mkpart primary ntfs 468993MiB 476161MiB
+parted -s "$DEV" name 5 "\"System Update 2\""
+mkntfs -q "${DEV}5" -f -L "System Update 2"
