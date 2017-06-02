adae3ac20c8e37370c53886e055f98b118d58c45
JW0914 <jw0914@outlook.com>
2017-06-02 10:31:08 -0500

    Wiki Collection
    Minor formatting changes

--- Scripts+Configs/ESD to WIM/DISM-Creation.cmd
+++ Scripts+Configs/ESD to WIM/DISM-Creation.cmd
@@ -28,7 +28,7 @@ cls
      set DEST=%CD%DISM
      set DESTL=%CD%DISM\en-us
 
-  :: Robocopy
+  :: Robocopy:
      set opt=/ETA /V /ZB
      set log=/NP /TEE /TS /LOG+:"%CD%DISM-RoboCopy_%date%_%dtime%.log"
 
@@ -41,7 +41,7 @@ cls
     :: Commands ::
 ::-------------------------------------
 
- :: Verify if Admin
+ :: Verify if Admin:
     %windir%\system32\reg.exe query "HKU\S-1-5-19" >nul 2>&1 || (
       cls
       color 4F
@@ -120,7 +120,7 @@ cls
     echo.
     echo                    Enter complete path to adksetup.exe file
     echo.
-    echo                        ^(with quotes if contain spaces^)
+    echo                       ^(with quotes if contains spaces^)
     echo.
     echo ===============================================================================
     echo.&echo.
@@ -223,7 +223,7 @@ cls
 
     robocopy %ADK%\DISM\ %DEST% %opt% %log%
 
- :: Language
+ :: Language:
     echo.&echo.&echo.&echo.&echo.
     echo ===============================================================================
     echo   ---------------------------------------------------------------------------
@@ -289,7 +289,7 @@ cls
 
         robocopy %ADK%\Wdsmcast\%lang%\ %DESTL% %opt% %log%
 
- :: Successful Completion
+ :: Successful Completion:
     color 2A
 
       echo.&echo.&echo.&echo.&echo.&echo.
