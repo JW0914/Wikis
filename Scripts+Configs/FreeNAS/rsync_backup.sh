3d9f1e76b03c69ce8ca70aed1d5aaafb23039e25
JW0914 <jw0914@live.com>
2020-01-30 08:34:12 -0600

    Create rsync_backup.sh
    Script for Backing up or Restoring via Rsync and emailing a status report when done

---
+++ Scripts+Configs/FreeNAS/rsync_backup.sh
@@ -0,0 +1,254 @@
+#!/bin/sh
+
+                 ##::[[-----> FreeNAS Rsync Backup <-----]]::##
+
+#==============================================================================
+                                 # Variables #
+#==============================================================================
+
+# Date & Time:
+  dt=$(date '+%Y.%m.%d_%H:%M:%S')
+  date=$(date '+%Y.%m.%d')
+
+# Email:
+  emailR="recipient@email.com"
+  emailS="sender@email.com"
+  subject="External Backup Status for ${hn}.${dn}"
+
+  # Device:
+    device="FreeNAS"
+    hn="echo $(hostname -s)"
+    dn="echo $(hostname -d)"
+
+#------------------------------------------------------------------------------
+# DataStores:
+
+  # Backups:
+    bak=backup-1
+
+  # Logging:
+    ssd=nas-ssd
+
+  # Storage:
+    data=nas-storage
+
+  # System:
+    sys=nas-system
+
+#------------------------------------------------------------------------------
+# Directories:
+
+  # Backup:
+    ext11=/mnt/${bak}/${data}/
+    ext12=/mnt/${bak}/${sys}/
+
+  # Data
+    zp1=/mnt/${data}/
+    zp2=/mnt/${sys}/
+
+  # Logs:
+    logfile=/tmp/Rsync_External_log.tmp
+    logs=/mnt/${ssd}/logs/rsync
+    extlog=/mnt/${bak}/logs
+
+#------------------------------------------------------------------------------
+# Rsync Logging:
+
+  # Backup:
+    logB1=--log-file=${logs}/backup/BACKUP_${data}-${dt}.log
+    logB2=--log-file=${logs}/backup/BACKUP_${sys}-${dt}.log
+
+    tailB1="tail -15 ${logs}/backup/BACKUP_${data}-${date}_**:**:**.log"
+    tailB2="tail -16 ${logs}/backup/BACKUP_${sys}-${date}_**:**:**.log"
+
+  # Restore:
+    logR1=--log-file=${logs}/restore/RESTORE_${data}-${dt}.log
+    logR2=--log-file=${logs}/restore/RESTORE_${sys}-${dt}.log
+
+    tailR1="tail -16 ${logs}/restore/RESTORE_${data}-${date}_**:**:**.log"
+    tailR2="tail -16 ${logs}/restore/RESTORE_${sys}-${date}_**:**:**.log"
+
+#------------------------------------------------------------------------------
+# Rsync:
+
+  # Options:
+    opt="-avrt --human-readable --partial --stats --info=progress2"
+
+  # Backup:
+    BackSt="rsync ${opt} ${logB1} ${zp1} ${ext11}"
+    BackSy="rsync ${opt} ${logB2} ${zp2} ${ext12}"
+
+  # Logs:
+    BackLog="rsync ${opt} ${logs} ${extlog}"
+
+  # Restore:
+    RestSt="rsync ${opt} ${logR1} ${ext11} ${zp1}"
+    RestSy="rsync ${opt} ${logR2} ${ext12} ${zp2}"
+
+
+#==============================================================================
+                                 # Paramaters #
+#==============================================================================
+
+# Email Options:
+#-------------------------------------------------------------------------------
+
+# Header:
+(
+  echo "To: ${emailR}"
+  echo "Subject: ${subject}"
+  echo "Content-Type: text/html"
+  echo "MIME-Version: 1.0"
+  printf "\r\n"
+) > ${logfile}
+
+# Begin HTML wrapper:
+  echo "<pre style=\"font-size:12px\">" >> ${logfile}
+
+# Heading:
+(
+  echo ""
+  echo "                ##[[-----> External Backup Status for FreeNAS <-----]]##                "
+  echo ""
+) >> ${logfile}
+
+
+# Storage Backup:
+#------------------------------------------------------------------------------
+
+# Copy:
+  printf "\n\n...Backing up NAS-Storage to Backup-1...\n\n"
+  ${BackSt}
+
+# Log:
+(
+  echo ""
+  echo ""
+  echo "    # NAS-Storage --> Backup-1 #                                                        "
+  echo "----------------------------------------------------------------------------------------"
+  echo ""
+  ${tailB1}
+  echo ""
+  echo ""
+  echo "       *****    *****    *****    *****          *****    *****    *****    *****       "
+) >> ${logfile}
+
+
+# System Backup:
+#------------------------------------------------------------------------------
+
+# Copy:
+  printf "\n\n...Backing up NAS-System to Backup-1...\n\n"
+  ${BackSy}
+
+# Log:
+(
+  echo ""
+  echo ""
+  echo "    # NAS-System --> Backup-1 #                                                         "
+  echo "----------------------------------------------------------------------------------------"
+  echo ""
+  ${tailB2}
+  echo ""
+  echo ""
+  echo "       *****    *****    *****    *****          *****    *****    *****    *****       "
+) >> ${logfile}
+
+
+#==============================================================================
+# Storage Restore:
+#------------------------------------------------------------------------------
+
+# Copy:
+##  printf "\n\n...Restoring NAS-Storage from Backup-1...\n\n"
+##  $RestSt
+
+# Log:
+##(
+##  echo ""
+##  echo ""
+##  echo "    # NAS-Storage <-- Backup-1 #                                                        "
+##  echo "----------------------------------------------------------------------------------------"
+##  echo ""
+##  ${tailR1}
+##  echo ""
+##  echo ""
+##  echo "       *****    *****    *****    *****          *****    *****    *****    *****       "
+##) >> ${logfile}
+
+
+# System Restore:
+#------------------------------------------------------------------------------
+
+# Copy:
+##  printf "\n\n...Restoring NAS-System from Backup-1...\n\n"
+##  $RestSy
+
+# Log:
+##(
+##  echo ""
+##  echo ""
+##  echo "    # NAS-System <-- Backup-1 #                                                         "
+##  echo "----------------------------------------------------------------------------------------"
+##  echo ""
+##  ${tailR2}
+##  echo ""
+##  echo ""
+##  echo "       *****    *****    *****    *****          *****    *****    *****    *****       "
+##) >> ${logfile}
+#==============================================================================
+
+
+# End HTML wrapper:
+  echo "</pre>" >> ${logfile}
+
+
+# Copy Logs:
+#------------------------------------------------------------------------------
+printf "\n\n...Backing up Logs to Backup-1...\n\n"
+${BackLog}
+
+
+# Email Report #
+#------------------------------------------------------------------------------
+
+# SendMail:
+  if [ -z "${emailR}" ]; then
+    echo "No email address specified, information available in ${logfile}"
+  else
+    sendmail ${emailR} < ${logfile}
+  fi
+
+
+#==============================================================================
+# SSMTP:
+
+  # Create Config:
+##    if [ -z "${emailR}" ]; then
+##      echo "No email address specified, information available in ${logfile}"
+##    else
+##      if [ ! -e ~/ssmtp.conf ]; then
+##        cat <<EOF > ~/ssmtp.conf
+##root=DEVICE@HN.DN
+##mailhub=smtp.email.com:587
+##rewriteDomain=email.com
+##AuthUser=SENDER
+##AuthPass=passphrase
+##FromLineOverride=YES
+##UseSTARTTLS=YES
+##EOF
+        # Insert variable values:
+##          sed -i "s/DEVICE/${device}/g" ~/ssmtp.conf
+##          sed -i "s/DN/${dn}/g" ~/ssmtp.conf
+##          sed -i "s/HN/${hn}/g" ~/ssmtp.conf
+##          sed -i "s/SENDER/${emailS}/g" ~/ssmtp.conf
+##      fi
+##    fi
+
+  # Email:
+##    ssmtp -v -C ~/ssmtp.conf ${emailR} < ${logfile}
+#==============================================================================
+
+
+# Cleanup:
+  rm ${logfile}
